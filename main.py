b0fd1f9dda4fbcacef63afd098781435ed5840f6
Matthew Pitropov <matthewpitropov@gmail.com>
2015-10-08 17:52:51 -0400

    Fixed Comments and made global vars

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 
 import sys
 
+queueType = 0;
+ticks = 0;
+lambdaVar = 0;
+L = 0;
+C = 0;
+
 def main():
         queueType = int(input("0 for M/D/1 or K for M/D/1/K: "))
         ticks = int(input("enter tick amount: "))
@@ -18,28 +24,30 @@ def main():
 		
 		t_arrival = calc_arrival_time
 		
-		for i in range(0,num_of_ticks):
+		for i in range(0,ticks):
 			arrival()
 			departure()
 
 		create_report();
 
 def calc_arrival_time():
-	u = rand(0,1) //generate random number between 0...1
+	u = rand(0,1) #generate random number between 0...1
 	arrival_time = ((-1/lambdaVar)*log(1-u) * 1000000)
 	return arrival_time
 
 def arrival():
-	if(t>=packet_arrival_time):
+	if( t >= packet_arrival_time):
 		packet_queue.add(new_packet)
 		t_arrival=t + calc_arrival_time()
 		t_departure= t+ (packet_size/transmission_rate)
-		//Also need to consider packet loss case when queue is full
+		#Also need to consider packet loss case when queue is full
 
 
 def departure():
 	if(t>=t_departure):
 		queue.pop()
 
+def create_report():
+	print("Data")
 
 main();
