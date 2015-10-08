83c3ce7694d8083fa650573845af20538a3e8a15
Matthew Pitropov <matthewpitropov@gmail.com>
2015-10-08 17:43:46 -0400

    Created base program

---
+++ main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import sys
+
+def main():
+        queueType = int(input("0 for M/D/1 or K for M/D/1/K: "))
+        ticks = int(input("enter tick amount: "))
+        # Average number of packets generated /arrived (packets per second) 
+        lambdaVar = int(input("enter lambda: "))
+        # Length of a packet in bits 
+        L = input("Enter L: ")
+        # The service time received by a packet.
+        # (Example: The transmission rate of the output link in bits per second.) 
+        C = input("Enter C: ")
+
+        if queueType != 0:
+                # Create K length Queue
+		
+		t_arrival = calc_arrival_time
+		
+		for i in range(0,num_of_ticks):
+			arrival()
+			departure()
+
+		create_report();
+
+def calc_arrival_time():
+	u = rand(0,1) //generate random number between 0...1
+	arrival_time = ((-1/lambdaVar)*log(1-u) * 1000000)
+	return arrival_time
+
+def arrival():
+	if(t>=packet_arrival_time):
+		packet_queue.add(new_packet)
+		t_arrival=t + calc_arrival_time()
+		t_departure= t+ (packet_size/transmission_rate)
+		//Also need to consider packet loss case when queue is full
+
+
+def departure():
+	if(t>=t_departure):
+		queue.pop()
+
+
+main();
